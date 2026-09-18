fcd316f51e0aa91fc08be0f882aa8d46f13f4b1b
leomessilibra45 <leomessilibra45@gmail.com>
2026-09-19 01:55:04 +0530

    Update Bing, Paradise, RT_DEV, Tear drivers

---
+++ Tear/6.6.sh
@@ -0,0 +1,6 @@
+#!/system/bin/sh
+# TearGame driver loader (repackaged for app auto-load) - upstream: t.me/TearGame
+DIR="$(dirname "$0")"
+insmod "$DIR/6.6.ko"
+echo "[tear] insmod 6.6.ko rc=$?"
+lsmod | grep -i Tear
